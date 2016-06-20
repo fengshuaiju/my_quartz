b4cafec5a474915724f7f4daa1a4ad1b1c787a1c
fengshuaiju <fengshuaiju@1yd.me>
2016-06-21 00:00:59 +0800

    接口返回json 调好了

--- src/main/java/com/feng/controller/HelloController.java
+++ src/main/java/com/feng/controller/HelloController.java
@@ -1,28 +1,32 @@
 package com.feng.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.feng.service.HelloService;
 
-@Controller
+@RestController
 public class HelloController {
 	
 	@Autowired
 	private HelloService helloService;
 	
 	@RequestMapping("/hello")
-	public String toSay(@RequestParam(required=false) String name,Model model){
+	public List<String> toSay(){
 		
+		List<String> list = new ArrayList<String>();
+		list.add("StringName");
+		list.add("StringNamePackage");
+		list.add("String_name");
+		list.add("String4_name_package");
 		
-		String sayHello = helloService.sayHello(name);
-		
-		model.addAttribute("say", sayHello);
-		
-		return "hello/hello";
+		return list;
 	}
 
 }
